89c3d290c47b6a49c8bcd5fef5ec0026adcd4f98
David Lee <davidlee980804@gmail.com>
2021-01-21 14:40:21 -0800

    Updated the editable todo list

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -15,44 +15,65 @@ import EditableTodoList from "./EditableTodoList";
  * App -> TodoApp -> { TodoForm, EditableTodoList }
  */
 
-function TodoApp() {
+function TodoApp({ initialTodos = [] }) {
+  const [todos, setTodos] = useState(initialTodos);
 
   /** add a new todo to list */
   function create(newTodo) {
+    setTodos(todos => (
+      [...todos, newTodo]
+    ));
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
+    setTodos(todos => {
+      return todos.map(todo => {
+        if (todo.id === updatedTodo.id) {
+          return updatedTodo;
+        } else {
+          return { ...todo };
+        }
+      })
+    });
   }
 
   /** delete a todo by id */
   function remove(id) {
+    setTodos(todos => (
+      todos.filter(todo => todo.id !== id)
+    ));
   }
 
+  let editableTodo = (todos.length !== 0)
+  ? <EditableTodoList />
+  : <span className="text-muted">You have no todos.</span>;
+
+  let topTodo = (todos.length !== 0)
+  ? (<section className="mb-4">
+      <h3>Top Todo</h3>
+      <TopTodo todos={todos} update={update} remove={remove} />
+    </section>)
+  : null;
+
   return (
-      <main className="TodoApp">
-        <div className="row">
+    <main className="TodoApp">
+      <div className="row">
 
-          <div className="col-md-6">
-            <EditableTodoList /> OR
-            <span className="text-muted">You have no todos.</span>
-          </div>
+        <div className="col-md-6">
+          {editableTodo}
+        </div>
 
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
-            <section className="mb-4">
-              <h3>Top Todo</h3>
-              <TopTodo />
-            </section>
-
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
+        <div className="col-md-6">
+          {topTodo}
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
               FIXME
             </section>
-          </div>
-
         </div>
-      </main>
+
+      </div>
+    </main >
   );
 }
 
